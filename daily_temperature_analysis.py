7cd6570a8e2f6c20eea933b17f7488edc092165c
Pizofreude <108355948+pizofreude@users.noreply.github.com>
2023-08-08 22:34:24 +0800

    daily_temperature_analysis.py: New config - created src/plot directory - set directory check for plot - autosave on

---
+++ daily_temperature_analysis.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# @Author: Pizofreude
+# @Date:   2023-08-08 00:00:00
+# Import libraries
+import os
+import random
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Generate random temperature data for a day
+# 24 hours
+# 20 - 35 °C
+# 24 rows
+# 8 columns
+# Will be replaced with actual data from Temperature API like Open-Meteo API (FOSS)
+temperature_data = [random.randint(20, 35) for _ in range(24)]
+
+# Create a DataFrame
+df = pd.DataFrame({'Hour': list(range(1, 25)), 'Temperature': temperature_data})
+
+# Create a temperature plot
+plt.plot(df['Hour'], df['Temperature'])
+plt.xlabel('Hour')
+plt.ylabel('Temperature (°C)')
+plt.title('Daily Temperature Analysis')
+
+# Create the directory if it doesn't exist
+os.makedirs('src/plot', exist_ok=True)
+
+# Save the plot image in the new directory
+plt.savefig('src/plot/temperature_plot.png')
+
+# Deactivate the virtual environment
+os.system('deactivate')
